9d75e0db94a5e15556b73b2e24d91f6c8b09fb20
Sebastian Triviño <justonly678@gmail.com>
2022-09-19 14:55:15 -0500

    mato el proceso despues de cierto tiempo 'after'

--- lib/sophos_app/fibonacci_server.ex
+++ lib/sophos_app/fibonacci_server.ex
@@ -2,10 +2,18 @@ defmodule SophosApp.Fibonacci_Server do
 
   def loop() do
     receive do
-      message -> IO.puts(message)
-        # code
+      {:sequence, n} ->
+        Fibonacci.sequence(n)
+        |> IO.inspect()
+
+      {:status, msg} ->
+        IO.puts("Running #{inspect(msg)}")
+
+      message ->
+        IO.inspect(message)
+
+      after
+        1500 -> IO.puts("se acabó")
     end
-
   end
-
 end
