dd8d8730c21730d986a96b2ea05faadc60078fa9
Sebastian Triviño <justonly678@gmail.com>
2022-09-19 16:29:15 -0500

    agrego el modulo pmap (fixed)

--- lib/sophos_app/parallel_list.ex
+++ lib/sophos_app/parallel_list.ex
@@ -3,6 +3,7 @@ defmodule SophosApp.ParallelList do
   def pmap(list, fun) do
     list
     |> Enum.map(&spawn_process(&1, self(), fun))
+    |> Enum.map(&await/1)
   end
 
   defp spawn_process(e, caller, fun) do
